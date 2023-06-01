cf6cbc0678d2e56f67030afe54b3ad0806d029b3
rstarov <48753755+rstarov@users.noreply.github.com>
2023-06-01 15:18:16 +0300

    Update backend.tf

--- live.git/dev/backend/backend.tf
+++ live.git/dev/backend/backend.tf
@@ -4,6 +4,6 @@
 # own subscription
 subscription_id      = "dfe71886-976f-4ecd-9ff7-57380eea330c"
 resource_group_name  = "project-resources"
-storage_account_name = "jdoe"
+storage_account_name = "jdoe1"
 container_name       = "dev-env-state"
 key                  = "dev-env.tfstate"
